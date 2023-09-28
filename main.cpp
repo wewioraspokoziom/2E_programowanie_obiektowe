9a514e7e69b8fb365ebe72b147b0bc29c12b3d12
wewioraspokoziom <145669378+wewioraspokoziom@users.noreply.github.com>
2023-09-28 12:38:20 +0200

    zadanie 28.09.2023

---
+++ main.cpp
@@ -0,0 +1,49 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int liczba, liczba1, liczba2, znak;
+    cout << "Podaj 1 liczbe " << endl;
+    cin >> liczba1;
+    cout << "Podaj 2 liczbe" << endl;
+    cin >> liczba2;
+    cout << "Wybierz jakie dzialanie chcesz zrobic: " << endl;
+    cout << "1 - dodawanie" << endl;
+    cout << "2 - odejmowanie" << endl;
+    cout << "3 - mnozenie" << endl;
+    cout << "4 - dzielenie" << endl;
+    cin >> znak; 
+    
+    if(znak == 1)
+    {
+        liczba = liczba1 + liczba2;
+        cout << liczba << endl;
+    }
+    else if(znak == 2)
+    {
+        liczba = liczba1 - liczba2;
+        cout << liczba << endl;
+    }
+    else if(znak == 3)
+    {
+        liczba = liczba1 * liczba2;
+        cout << liczba << endl;
+    }
+    else if(znak == 4)
+    {
+        liczba = liczba1 / liczba2;
+        cout << liczba << endl;
+    }
+    else if(znak == 0)
+    {
+        exit;
+    }
+    else 
+    {
+        cout << "błędna cyfra" << endl;
+    }
+    return 0;
+}
