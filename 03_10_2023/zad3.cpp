8d81c59f42c8034bb613de3c70c314433391a4a4
wewioraspokoziom <rilskyxz@gmail.com>
2023-10-03 15:12:45 +0200

    Zadanie 3 zrobione

---
+++ 03_10_2023/zad3.cpp
@@ -0,0 +1,24 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+
+    cout <<  "Podaj wartosci podanych bokow" << endl;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+
+    if (a + b > c && a + c > b && b + c > a)
+    {
+        cout << "Z tych bokow da sie zrobic trojkat" << endl;
+    }
+    else
+        cout << "Z tych bokow nie da sie zrobic trojkata " << endl;
+    
+}
