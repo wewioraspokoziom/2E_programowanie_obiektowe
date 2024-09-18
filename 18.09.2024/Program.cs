cf4c1920b5d249f7473c8b29dc0b3834d643e3ce
wewioraspokoziom <145669378+wewioraspokoziom@users.noreply.github.com>
2024-09-18 13:16:01 +0200

    Add files via upload

---
+++ 18.09.2024/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+public class Program
+{
+    public static void Main(string[] arqs)
+    {
+        Console.WriteLine("Zadanie 1");
+        double pi = 3.14;
+        Console.WriteLine(pi);
+
+        Console.WriteLine("Zadanie 2");
+        string imie = "Maciej";
+        string nazwisko = "Stepien";
+        Console.WriteLine(imie + " " + nazwisko);
+
+        Console.WriteLine("Zadanie 3");
+        int x = 10;
+        double y = Convert.ToDouble(x);
+        Console.WriteLine(y);
+
+        Console.WriteLine("Zadanie 4");
+        double a = 10.78493;
+        int b = Convert.ToInt32(a);
+        Console.WriteLine(b);
+
+        Console.WriteLine("Zadanie 5");
+        Console.WriteLine("Podaj napis");
+        string napis = Console.ReadLine();
+        Console.WriteLine(napis);
+    }
+}
